ca06b7ec85a36dac8aa027bd340b170971837467
Susan <susandu924@gmail.com>
2022-09-20 01:52:10 -0500

    Part 6 finished

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -2,28 +2,12 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class Employer {
+public class Employer extends JobField {
 
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public Employer() {
-        id = nextId;
-        nextId++;
+    public Employer (String value) {
+        super(value);
     }
-
-    public Employer(String value) {
-        this();
-        this.value = value;
-    }
-
     // Custom toString, equals, and hashCode methods:
-
-    @Override
-    public String toString() {
-        return value;
-    }
 
     @Override
     public boolean equals(Object o) {  // Two objects are equal if they have the same id.
@@ -37,19 +21,4 @@ public class Employer {
     public int hashCode() {
         return Objects.hash(getId());
     }
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
 }
